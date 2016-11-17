b46397f21851d271466d5ae414ae15199798f6ca
bochm <bochm@163.com>
2016-11-17 20:43:42 +0800

    bcm home

---
+++ src/main/java/cn/bx/xweb/controller/PagesController.java
@@ -0,0 +1,39 @@
+package cn.bx.xweb.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.HandlerMapping;
+
+import cn.bx.system.entity.User;
+import cn.bx.system.utils.UserUtils;
+
+
+@Controller
+public class PagesController {
+	Logger log  = Logger.getLogger(PagesController.class);
+	@Value("${login.url}")
+	private String loginUrl;
+	@Value("${index.url}")
+	private String indexUrl;
+	@Value("${pages.url}")
+	private String pagesUrl;
+	@RequestMapping("/index")
+	public String index(){
+		return indexUrl;
+	}
+	@RequestMapping("/login")
+	public String login(){
+		User user = UserUtils.getUser();
+		return (user != null && user.getId() != null) ? indexUrl : loginUrl;
+	}
+	@RequestMapping("${pages.url}**")
+	public String pages(HttpServletRequest request) {
+		String requestUrl = (String)request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+		log.debug("jsp请求:"+requestUrl);
+		return requestUrl.replaceFirst(pagesUrl, "");
+	}
+}
